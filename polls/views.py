ef5c248e626767643d3fbe0ea2c9ec373b747a66
skolanko_ubuntu <skolanko222@gmail.com>
2022-12-07 20:20:19 +0100

    cleaning bugfixes

--- polls/views.py
+++ polls/views.py
@@ -11,22 +11,20 @@ def index(request):
     return render(request, 'polls/index.html', context) # a shortcut for rendering templates by HttpResponse
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk = question_id)
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
-    question = get_object_or_404(Question, pk = question_id)
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question':question})
 
 def vote(request, question_id):
-    
-    question = get_object_or_404(Question, pk = question_id) # getting the object user want to vote on
-    
+    question = get_object_or_404(Question, pk = question_id)
     try:
         choice = question.choice_set.get(pk=request.POST['choice'])
     except(KeyError,Choice.DoesNotExist): # exception, when no option is choosen
        return  render(request, 'polls/detail.html', {
-            'question': question, 
+            'question': question,
             'error_message': "You didn't select a choice.",})
     
     else:
